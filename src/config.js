0b9ba30a395f6b857a6557f6292d48e085eaa076
Jeremy Brown <jlbmagic@gmail.com>
2023-01-18 10:41:29 -0500

    first set up

---
+++ src/config.js
@@ -0,0 +1,34 @@
+
+
+export const mapping = {
+    PrimaryKey: "id",
+    "proj_PER::NameFull_ct": "text",
+    JobDate__cd: "start_date",
+    Color: "$color",
+    Description__lu: "text",
+    "Description_lu": "details",
+    "_kf_TransferMethodReceiptID": "section",
+}
+export const colors = {
+    1: "red",
+    2: "#00FF00",
+    3: "#0000FF",
+    4: "#FFFF00",
+    5: "#FF00FF",
+    6: "#00FFFF",
+    7: "#000000",
+    8: "#FFFFFF",
+    9: "#FF8000",
+    10: "#FF0080",
+    11: "#8000FF",
+    12: "red",
+    13: "#00FF80",
+    14: "#80FF00",
+    15: "#FF8000",
+    16: "#FF0080",
+    17: "#8000FF",
+    18: "#0080FF",
+    19: "#00FF80",
+    20: "#80FF00",
+    
+}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,146 @@
-//Here we're importing items we'll need. You can add other imports here.
+import {colors, mapping} from "./config.js";
 
-//The first function. Remove this.
-const btn = document.querySelector("button");
-btn.onclick = function () {
-  alert("You ran some JavaScript");
-};
+import fmGofer from "fm-gofer";
+
+// import {scheduler} from "bundle-text:/src/scheduler/codebase/scheduler.js";
+// import {webix} from "bundle-text:/src/webix/codebase/webix.js";
+
+// window.loadCalendar = function () {
+
+window.loadSchedule = (json) => {
+  const obj = JSON.parse(json);
+  const width = obj.width;
+  console.log("width", width)
+class CustomBarsView extends scheduler.views['bars/nav'] {
+  config() {
+    const ui = super.config();
+    ui.width = 100;
+      ui.options = [
+          { id: 'timeline', value: 'Work Load' },
+      ];
+      return ui;
+  }
+}
+class CustomHours extends scheduler.views["modes/common/hourscale"] {
+  config() {
+    const ui = super.config();
+    ui.type.height = 520;
+    return ui;
+  }
+  ParseHours() {
+    const data = [];
+    for (let h = 0; h < 24; h++) {
+      if (h > 7 && h < 22) data.push({ id: h + "" });
+    }
+    this.List.parse(data);
+  }
+}
+class CustomBarView extends scheduler.views["modes/timeline/bar"] {
+  config() {
+    const ui = super.config();
+    const rich = ui.elements[1];
+    console.log("RICH", rich)
+    rich.options.push({ id: "quarter", value: "Quarter" });
+    rich.options = rich.options.filter((item) => item.id !== "day");
+    return ui;
+  }
+}
+class CustomTimelineView extends scheduler.views["modes/timeline"] {
+  GetScalesArray(type) {
+    if (type === "quarter") return [{ unit: "month", format: "%F %Y" }];
+    return super.GetScalesArray(type);
+  }
+
+  GetScalesCellWidth(type) {
+    if (type === "quarter") return 500;
+    return super.GetScalesCellWidth(type);
+  }
+}
+
+class MyBackend extends scheduler.services.Backend {
+  // sections() {
+  //   return sections
+  // }
+    async addEvent(event) {
+      const action = "add";
+      const obj = {action, event};
+      const result = await fmGofer.PerformScript("HandleEvents", JSON.stringify(obj));
+      return result;
+    }
+    async updateEvent(id, newData) { 
+      const action = "update";
+      const obj = {action, id, newData};
+      const result = await fmGofer.PerformScript("HandleEvents", obj);
+      console.log(result)
+      return result;
+    }
+    // const sections = { id: 1, text: "Section 1" };
+
+    async events() {
+      const action = "get";
+      const obj = {action}
+      const result = await fmGofer.PerformScript("HandleEvents", JSON.stringify(obj));
+      console.log("RESULT",result);
+      const events = JSON.parse(result);
+      console.log(events[0])
+      const newEvents = events.map((event) => {
+        const keys = Object.keys(event.fieldData);
+        const newEvent = {}
+        
+        keys.forEach((key) => {
+          const newKey = mapping[key]
+          if (newKey) {
+            newEvent[newKey] = event.fieldData[key]
+          }
+          if (newKey === "start_date") {
+            newEvent.start_date = new Date(event.fieldData[key])
+            newEvent.end_date = new Date(event.fieldData[key])
+           
+            // newEvent.text = newEvent.details || "No Details";
+            newEvent.color = colors[event.fieldData._kf_StatusID] || "#000000";
+            newEvent.all_day= 1
+          }
+          })
+          return newEvent
+      })
+      console.log(newEvents)
+      return newEvents;
+    }
+    async calendars() {
+      const action = "getCalendars";
+      const obj = {action}
+      const result = await fmGofer.PerformScript("HandleEvents",JSON.stringify(obj));
+      return JSON.parse(result);
+    }
+    async units() {
+      const action = "getUnits";
+      const obj = {action}
+      const result = await fmGofer.PerformScript("HandleEvents",JSON.stringify(obj));
+      return JSON.parse(result);
+  }
+  async sections() {
+      const action = "getSections";
+      const obj = {action}
+      const result = await fmGofer.PerformScript("HandleEvents",JSON.stringify(obj));
+      return JSON.parse(result);
+    }
+  }
+
+  webix.CustomScroll.init();
+  const mySched = webix.ui({
+    view: "scheduler",
+    calendars:true,
+    day:true,
+    timeline:true,
+    url: "http://localhost:1234/",
+    override: new Map([
+      [scheduler.services.Backend, MyBackend],
+      [scheduler.views["modes/common/hourscale"], CustomHours],
+     
+      [scheduler.views["modes/timeline/bar"], CustomBarView],
+      [scheduler.views["modes/timeline"], CustomTimelineView],
+    ]),
+  });
+  // mySched.define("width", width)
+  
+}
